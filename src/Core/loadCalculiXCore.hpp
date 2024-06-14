5e3b4106de986eadc7ff1268bf2930a5d6e56e6c
NorbertHofbauer <95526692+NorbertHofbauer@users.noreply.github.com>
2024-06-15 01:46:52 +0200

    windows

--- src/Core/loadCalculiXCore.hpp
+++ src/Core/loadCalculiXCore.hpp
@@ -3,6 +3,15 @@
 
 #include "CalculiXCore.hpp"
 
-extern CalculiXCore ccx_core;
+#ifdef WIN32
+    #if COMPILING_THE_DLL
+        #define DLLEXTERN __declspec(dllexport)
+    #else
+        #define DLLEXTERN __declspec(dllimport)
+    #endif
+    DLLEXTERN CalculiXCore ccx_core;
+#else
+    extern CalculiXCore ccx_core;
+#endif
 
 #endif // LOADCALCULIXCORE_HPP
